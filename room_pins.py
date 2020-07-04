884bf0ce59fc0a297d0dccf655132a274f454c88
Chris Calef <chris.calef@gmail.com>
2020-07-03 21:12:06 -0700

    debugging

--- room_pins.py
+++ room_pins.py
@@ -11,6 +11,7 @@ print("Copying pins for rooms file: " + rooms_file)
 with open(rooms_file, "r") as read_file:
     rooms_data = json.load(read_file)
     read_file.close()
+    
 
 db = psycopg2.connect(host="localhost",database="polycosm_production",user="postgres")
 if db is None:
@@ -20,6 +21,8 @@ if db is None:
 c = db.cursor()
 for room in rooms_data["rooms"]:
     print("slug: " + room["slug"] + " SID: " + room["hub_sid"])
+    for copy_room in room["copy_rooms"]:
+        print("Copy room: " + copy_room_
     sql = "SELECT hub_id FROM hubs WHERE hub_sid='" + room["hub_sid"] + "';"
     c.execute(sql)
     hub_id = c.fetchone()[0]
