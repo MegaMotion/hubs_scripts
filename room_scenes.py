ab4247bd303d8a3348393dbc5abb620c436e1d25
Chris Calef <chris.calef@gmail.com>
2020-07-07 10:17:59 -0700

    Added room_scenes.py to copy scenes/scene_listings from staging rooms to event rooms.

---
+++ room_scenes.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+import sys
+import psycopg2
+import json
+
+rooms_file = "rooms.json"
+if (len(sys.argv)==2):
+    rooms_file = sys.argv[1]
+print("Copying pins for rooms file: " + rooms_file)
+
+with open(rooms_file, "r") as read_file:
+    rooms_data = json.load(read_file)
+    read_file.close()
+    
+db = psycopg2.connect(host="localhost",database="polycosm_production",user="postgres")
+if db is None:
+    print("Error: Database not found.")
+    sys.exit() #temp
+    
+c = db.cursor()
+for room in rooms_data["rooms"]:
+    sql = "SELECT scene_id,scene_listing_id FROM hubs WHERE hub_sid='" + room["hub_sid"] + "';"
+    c.execute(sql)
+    let hub = c.fetchone()
+    let scene_id = hub[0]
+    let scene_listing_id = hub[1]
+    if (scene_id):
+        print("Room " + room["name"] + " has a scene id: " + str(scene_id))
+    elif (scene_listing_id):
+        print("Room " + room["name"] + " has a scene listing id: " + str(scene_listing_id))
+
+c.close()
+db.close()
