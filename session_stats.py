249a388cda28808366dd95369f17c5da53df8c49
Chris Calef <chris.calef@gmail.com>
2021-03-07 18:03:31 -0800

    ...

--- session_stats.py
+++ session_stats.py
@@ -40,10 +40,10 @@ while h < 18: # for a one hour event starting at five pm, ie check 17:00 to 19:0
         attempt_one = payload['userAgent']
         #attempt_two = payload[0]
         encodedUnicode = json.dumps(payload)
-        userAgent = encodedUnicode['userAgent']
+        #userAgent = encodedUnicode['userAgent']
         #attempt_three = payload[u'userData']        
         #json_payload = json.loads(str(payload))
-        print "ID: " + str(id) + ",  userAgent:   " + str(encodedUnicode)
+        print "ID: " + str(id) + ",  userAgent:   " + attempt_one
 
 #session_length_query = "SELECT session_id,started_at,ended_at,AGE(ended_at,started_at),entered_event_payload FROM session_stats WHERE started_at::date>='" + start_date + "' AND started_at<='" + end_date  + "';"
 #  ORDER BY AGE(ended_at,started_at) DESC;"
