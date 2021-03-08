a75aaa97145131c3a54c13a71d4e73e1f6e15f30
Chris Calef <chris.calef@gmail.com>
2021-03-07 21:08:05 -0800

    ...

--- session_stats.py
+++ session_stats.py
@@ -37,14 +37,14 @@ while h < 18: # for a one hour event starting at five pm, ie check 17:00 to 19:0
     for row in rows:
         id = row[0]
         payload = json.loads(row[1])
-        #encodedUnicode = json.dumps(payload)
+        payload_dump = json.dumps(payload)
         #userAgent = encodedUnicode['userAgent']
         #attempt_three = encodedUnicode['userAgent']       
         #json_payload = json.loads(encodedUnicode)
         #payload_s = json.dumps(json_payload)
-        occupantCount = payload["initialOccupantCount"]
+        #occupantCount = payload["initialOccupantCount"]
         #occupantCount = json_payload["initialOccupantCount"]
-        print "ID: " + str(id) + ",  userAgent:   " + str(occupantCount)
+        print "ID: " + str(id) + ",  payload dump:   " + payload_dump
 
 #session_length_query = "SELECT session_id,started_at,ended_at,AGE(ended_at,started_at),entered_event_payload FROM session_stats WHERE started_at::date>='" + start_date + "' AND started_at<='" + end_date  + "';"
 #  ORDER BY AGE(ended_at,started_at) DESC;"
