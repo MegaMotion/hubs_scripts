3e35243c0fc2f9b36336d9a9f7a461cad2078577
Chris Calef <chris.calef@gmail.com>
2021-03-07 21:12:24 -0800

    ...

--- session_stats.py
+++ session_stats.py
@@ -38,7 +38,7 @@ while h < 18: # for a one hour event starting at five pm, ie check 17:00 to 19:0
         id = row[0]
         payload = row[1]
         payload_dump = json.dumps(payload)
-        occupantCount = payload["initialOccupantCount"]
+        occupantCount = payload[0]
         print "ID: " + str(id) + ",  payload dump:   " + payload_dump
 
 #session_length_query = "SELECT session_id,started_at,ended_at,AGE(ended_at,started_at),entered_event_payload FROM session_stats WHERE started_at::date>='" + start_date + "' AND started_at<='" + end_date  + "';"
