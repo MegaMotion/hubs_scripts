0743e4b2a1827c08007e78405d600f4c77a5c141
Chris Calef <chris.calef@gmail.com>
2021-03-07 21:19:15 -0800

    ...

--- session_stats.py
+++ session_stats.py
@@ -40,7 +40,7 @@ while h < 18: # for a one hour event starting at five pm, ie check 17:00 to 19:0
         if payload is not None:
             for key, value in payload.iteritems() :
                 print key, value
-            occupantCount = payload[0]
+            occupantCount = payload["initialOccupantCount"]
             print "ID: " + str(id) + ",  occupantCount:   " + str(occupantCount)
 
 #session_length_query = "SELECT session_id,started_at,ended_at,AGE(ended_at,started_at),entered_event_payload FROM session_stats WHERE started_at::date>='" + start_date + "' AND started_at<='" + end_date  + "';"
