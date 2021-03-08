97d3e2a79004a6eb5bcf2d6f494f1e0cfa85696c
Chris Calef <chris.calef@gmail.com>
2021-03-07 18:06:46 -0800

    ...

--- session_stats.py
+++ session_stats.py
@@ -37,7 +37,7 @@ while h < 18: # for a one hour event starting at five pm, ie check 17:00 to 19:0
     for row in rows:
         id = row[0]
         payload = row[1]
-        attempt_one = payload['userAgent']
+        attempt_one = payload["userAgent"]
         #attempt_two = payload[0]
         encodedUnicode = json.dumps(payload)
         #userAgent = encodedUnicode['userAgent']
