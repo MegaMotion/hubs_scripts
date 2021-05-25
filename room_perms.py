04560e5a3fbe3bde105cb871909f6f37f3bdb644
Chris Calef <chris.calef@gmail.com>
2021-05-25 11:05:13 -0700

    Added room_perms to create ownership for entity members across all rooms.

---
+++ room_perms.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+import sys
+import psycopg2
+import json
+import requests
+
+entities_file = "entities.json" # new way
+r = requests.get('https://hubbub6-data.s3-us-west-2.amazonaws.com/entities.json') # newest way
+entities_data = json.loads(r.text)
+
+
+db = psycopg2.connect(host="localhost",database="polycosm_production",user="postgres")
+if db is None:
+    print("Error: Database not found.")
+    sys.exit() #temp
+    
+c = db.cursor()
+
+# Task: to add a row in hub_role_memberships for each entity member, for each room, both staging and event.
+rooms = []
+for entity in entities_data["entities"]:
+  for room in entity["rooms"]:
+    sql = "SELECT hub_id FROM hubs WHERE hub_sid='" + room["staging"] + "';"
+    c.execute(sql)
+    rooms.append(c.fetchone()[0])
+    
+    for event_room in room["event"]:
+      print("Event room: " + event_room)
+      sql = "SELECT hub_id FROM hubs WHERE hub_sid='" + event_room + "';"
+      c.execute(sql)
+      rooms.append(c.fetchone()[0])
+
+for room in rooms:
+  print("Room: " + room)
+
+db.commit()    
+c.close()
+db.close()
--- room_pins.py
+++ room_pins.py
@@ -9,18 +9,6 @@ entities_file = "entities.json" # new way
 r = requests.get('https://hubbub6-data.s3-us-west-2.amazonaws.com/entities.json') # newest way
 entities_data = json.loads(r.text)
 
-#if (len(sys.argv)==2):
-#    rooms_file = sys.argv[1]
-#print("Copying pins for rooms file: " + rooms_file)
-
-#with open(rooms_file, "r") as read_file:
-#    rooms_data = json.load(read_file)
-#    read_file.close()
-
-#with open(entities_file, "r") as read_file:
-#    entities_data = json.load(read_file)
-#    read_file.close()
-
 do_not_copy = [] #  useful for signage or whatever you need in the staging rooms that you do not want in the event rooms.
 do_not_delete = [] # pinned items in event rooms that should not be overwritten from the staging rooms (for future)
 
@@ -64,35 +52,3 @@ for entity in entities_data["entities"]:
 db.commit()    
 c.close()
 db.close()
-# # Old way, using rooms.json:
-# for room in rooms_data["rooms"]:
-#     #print("slug: " + room["slug"] + " SID: " + room["hub_sid"])
-#     sql = "SELECT hub_id FROM hubs WHERE hub_sid='" + room["hub_sid"] + "';"
-#     c.execute(sql)
-#     hub_id = c.fetchone()[0]
-    
-#     sql = "SELECT * FROM room_objects WHERE hub_id=" + str(hub_id) + ";"
-#     c.execute(sql)
-#     object_rows = c.fetchall()
-#     print("Found " + str(len(object_rows)) + " objects in " + room["slug"])
-          
-#     for copy_room in room["copy_rooms"]:
-#         print("Copy room: " + copy_room)
-#         sql = "SELECT hub_id FROM hubs WHERE hub_sid='" + copy_room + "';"
-#         c.execute(sql)
-#         copy_hub_id = c.fetchone()[0]
-#         sql = "DELETE FROM room_objects WHERE hub_id=" + str(copy_hub_id) + ";"
-#         c.execute(sql)
-#         for row in object_rows:
-#             room_object_id = row[0]
-#             object_id = row[1]
-#             gltf_node = row[3]
-#             inserted_at = row[4]
-#             updated_at = row[5]
-#             account_id = row[6]
-#             #print("Object id: " + str(object_id))
-#             if object_id not in do_not_copy:
-#                 c.execute("INSERT INTO room_objects(object_id,hub_id,gltf_node,inserted_at,updated_at,account_id) " + \
-#                           "VALUES (%s,%s,%s,%s,%s,%s);",\
-#                           (object_id,copy_hub_id,psycopg2.Binary(gltf_node),inserted_at,updated_at,account_id))
-
