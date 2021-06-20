e9f9690f00f7ce9b188f47e53eae9671a5862e15
Chris Calef <chris.calef@gmail.com>
2021-06-20 11:17:45 -0700

    Added time component to cron test for unique key.

--- cron_test.py
+++ cron_test.py
@@ -3,12 +3,15 @@
 #import psycopg2
 import json
 import requests
+from datetime import datetime
 
 
 lambdaUrl = 'https://icswse9im8.execute-api.us-west-2.amazonaws.com/ocf/base'
+pk = 'Room-temp-' + datetime.now().strftime("%M:%S")
 temp_params = { 
   'command': 'addRooms',
-  'staging_rooms': [{'pk': 'Room-bbbbbbb',
+  'staging_rooms': [{
+    'pk': pk,
     'sk': 'TESTING',
     'data': 'bbbbbbb',
     'size': 35}],
@@ -17,5 +20,5 @@ temp_params = {
 
 print(json.dumps(temp_params))
 print("SENDING!!!")
-r = requests.post(lambdaUrl, json = temp_params)
-print(r.text)
+#r = requests.post(lambdaUrl, json = temp_params)
+#print(r.text)
